221b6209c180abffcb2c78818feb03f498cc55a3
Shanmukh Karra <shanmukh1608@gmail.com>
2019-08-20 13:34:27 +0530

    Fixed indenting in globals.h

--- globals.h
+++ globals.h
@@ -1,9 +1,9 @@
 #ifndef GLOBALS_H_INCLUDED
 #define GLOBALS_H_INCLUDED
 
-extern char* input;
-extern char* command;
-extern char* flags;
-extern char* arguments;
+extern char *input;
+extern char *command;
+extern char *flags;
+extern char *arguments;
 
 #endif
